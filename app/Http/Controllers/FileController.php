86f3cd06c8dc1dd70df6de2134fa1006d10eba86
valium69mg <xdeus.v2@gmail.com>
2024-09-15 15:12:41 -0600

    Add breeze for authentication and add migration, model and controller for files

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,46 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+    // receive a file
+    public function getFile(Request $request) {
+        dd($request);
     }
 }
---
+++ app/Models/File.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    use HasFactory;
+
+    // file params
+    protected $fillable = [
+        'name',
+        'extention',
+        'path',
+    ];
+}
