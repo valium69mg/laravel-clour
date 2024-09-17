8db2f0a049e4b64579f4f439ba210dc3d7f36cfd
valium69mg <xdeus.v2@gmail.com>
2024-09-16 18:57:15 -0600

    Add anchor tag that redirects to image or file path with a link

--- resources/views/folders/getFolder.blade.php
+++ resources/views/folders/getFolder.blade.php
@@ -64,7 +64,9 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" width="78" height="78" fill="gray" class="bi bi-file-earmark-fill" viewBox="0 0 16 16">
                                 <path d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2m5.5 1.5v2a1 1 0 0 0 1 1h2z"/>
                                 </svg>
+                                <a href="../{{$file->path}}">
                                 <h2> {{$file->name}} </h2> 
+                                </a>
                             </div>
                         @endforeach
                        
